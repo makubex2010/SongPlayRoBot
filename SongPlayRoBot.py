145455147ab350f899aa9ef1e7e967bbcd9b58f8
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-06-10 02:33:07 +0800

    Update SongPlayRoBot.py

--- SongPlayRoBot.py
+++ SongPlayRoBot.py
@@ -6,7 +6,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup
 )
 import yt_dlp
-from youtube-search-python import YoutubeSearch
+from youtubesearchpython import YoutubeSearch
 import requests
 
 import os
