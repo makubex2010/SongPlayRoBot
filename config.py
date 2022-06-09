46ad2da4435048c88dfdd39a7f556d8d4e9d05b8
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-06-10 03:26:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-class Config:
-    API_ID = "2710398"  # 🤖 與 APP_ID 相同。 從 My.telegram.org 獲取。
-    API_HASH = "e64c45a8f94ae642c090404e5f81f196" # 🤖 從 My.telegram.org 獲取。
-    BOT_TOKEN = "1800777638:AAFn7aEw4LV8kb7Cm7xexcGoXJoHf_0iuq4" # 🤖 通過發送 /newbot 命令從@BotFather（電報）獲取它。
+import os
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
