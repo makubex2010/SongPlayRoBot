8bd29ce066c9f7109827e85fe450ae96369e3017
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-06-10 02:17:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 與 APP_ID 相同。 從 My.telegram.org 獲取。
-    API_HASH = "" # 🤖 從 My.telegram.org 獲取。
-    BOT_TOKEN = "" # 🤖 通過發送 /newbot 命令從@BotFather（電報）獲取它。
+    API_ID = "2710398"  # 🤖 與 APP_ID 相同。 從 My.telegram.org 獲取。
+    API_HASH = "e64c45a8f94ae642c090404e5f81f196" # 🤖 從 My.telegram.org 獲取。
+    BOT_TOKEN = "1800777638:AAFn7aEw4LV8kb7Cm7xexcGoXJoHf_0iuq4" # 🤖 通過發送 /newbot 命令從@BotFather（電報）獲取它。
